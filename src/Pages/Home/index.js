fce88711d2a0ee264b1eba6f61e6d0920554e733
Edvan Jr <edvan.hd@gmail.com>
2022-01-07 02:20:37 -0300

    :sparkles: Responsividade

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,18 +2,7 @@ import './styles.css';
 
 function Home() {
   return (
-    <div className="container-home">
-      <div className="apresentacao">
-        <p>
-          Olá,<br />
-          seja bem vind@! <br />
-          Meu nome é Edvan Jr.,<br />
-          e amo desenvolver<br />
-          páginas web.<br />
-          👋
-        </p>
-      </div>
-    </div>
+    <div className="container-home" />
   );
 }
 
